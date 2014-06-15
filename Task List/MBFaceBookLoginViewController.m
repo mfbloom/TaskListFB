1248fe3f30a591a3489b2c50cf558156515cf029
Michael Bloom <michaelbloom@Michaels-MacBook-Pro.local>
2014-06-15 02:24:37 -0400

    FaceBook Login

--- Task List/MBFaceBookLoginViewController.m
+++ Task List/MBFaceBookLoginViewController.m
@@ -26,8 +26,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
+-(void)viewDidAppear:(BOOL)animated
+{
+    
+    FBLoginView *loginView =[[FBLoginView alloc] initWithReadPermissions:@[@"public_profile", @"email"]];
+    
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 30);
+    [self.view addSubview:loginView];
+    
+    loginView.delegate = self;
+    
+    if(FBSession.activeSession.isOpen)
+    {
+        [self performSegueWithIdentifier:@"toNavigationViewControllerFromFB" sender:self];
+    }
+}
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -35,15 +54,41 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    [FBLoginView class];
+        return YES;
+}
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
+
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+  
+    if([segue.destinationViewController isKindOfClass:[MBViewController class]])
+    {
+        MBViewController *nextVC = segue.destinationViewController;
+        
+        nextVC.delegate = self;
+    }
 }
-*/
 
+#pragma mark - FBLoginViewDelegate
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id)user {
+    self.nameLabel.text = ;
+    self.profilePic.profileID = user.id;
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    self.profilePic.profileID = nil;
+    self.usernameLabel.text = nil;
+}
 @end
